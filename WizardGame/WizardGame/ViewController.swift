5b529751843969fe5c1a5493baeac926d840930d
Alexis Saint-Jean <asaintjean@gmail.com>
2015-01-18 00:51:04 -0500

    Added UITextFieldDelegate to ViewController to dismiss the keyboard when hitting 'return' to type the name of the player in the top textField

--- WizardGame/WizardGame/ViewController.swift
+++ WizardGame/WizardGame/ViewController.swift
@@ -8,10 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var playerName: UITextField!
+    
+    // Function that ensures the keyboard disappear after the player enters 'Return'
+    // on the pop-up keyboard when they are done entering their name
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        textField.resignFirstResponder()
+        return true;
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.playerName.delegate = self;
         // Do any additional setup after loading the view, typically from a nib.
     }
 
