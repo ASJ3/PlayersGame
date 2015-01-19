98e3f135b171072905105d972aaaa70a5b7ff091
Alexis Saint-Jean <asaintjean@gmail.com>
2015-01-19 17:04:42 -0500

    Included a newMatch() variables. Also main function now has one more if condition when readyToPlay variable is equal to 2 (i.e. game has started)

--- WizardGame/WizardGame/ViewController.swift
+++ WizardGame/WizardGame/ViewController.swift
@@ -19,8 +19,16 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var computerPlayerHealth: UILabel!
     @IBOutlet weak var mainLabel: UITextView!
     
+    // Creating 4 global variables
     var player1 = Player(playerName: "")
     var player2 = Player(playerName: "")
+    
+    // *****************************************
+    // For some reason I can't instantiate Match() by using the player1 and player2
+    // variables as properties
+    // If I do I get an error message saying ViewController.Type does not have a member named...
+    var newMatch = Match(player1: Player(playerName: ""), player2: Player(playerName: ""))
+    
     // readyToPlay is a var created to indicate whether a name has been entered by the player
     // It starts at 0 (player did not enter name in the textField so game is not ready to play
     // Then it changes to 1 (game is ready)
@@ -43,8 +51,16 @@ class ViewController: UIViewController, UITextFieldDelegate {
             
             mainLabel.text = "Let the game begin!\n\(player1.name) from \(player1.school) will play against \(player2.name) from \(player2.school)"
             
-            readyToPlay = 0
+            readyToPlay = 2
             println("readyToPlay has been changed to: \(readyToPlay)")
+            
+        } else if readyToPlay == 2 {
+            
+            computerPlayerHealth.text = String(80)
+            
+            mainLabel.text = "Here is the game so far:\n\(player1.name) from \(player1.school) will play against \(player2.name) from \(player2.school)"
+            
+            readyToPlay = 0
             
         } else {
             println("readyToPlay is now: \(readyToPlay)")
