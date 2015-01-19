866d685f69bff8ab86563419a928012521208722
Alexis Saint-Jean <asaintjean@gmail.com>
2015-01-19 18:10:51 -0500

    Progress in the flow of the game: the rounds will alternate bw the human and computer players, with the health of each updated after each attack

--- WizardGame/WizardGame/Match.swift
+++ WizardGame/WizardGame/Match.swift
@@ -24,13 +24,16 @@ class Match {
         var startingPlayer: Player
         var followingPlayer: Player
         
-        if randomFirst == 1 {
-            startingPlayer = self.player1
-            followingPlayer = self.player2
-        } else {
-            startingPlayer = self.player2
-            followingPlayer = self.player1
-        }
+        startingPlayer = self.player1
+        followingPlayer = self.player2
+        
+//        if randomFirst == 1 {
+//            startingPlayer = self.player1
+//            followingPlayer = self.player2
+//        } else {
+//            startingPlayer = self.player2
+//            followingPlayer = self.player1
+//        }
         
         println("Let the game begin!! \(startingPlayer.name) will start. \(followingPlayer.name) will play second.")
         // Reset the health of each player to 100 in case we are playing
@@ -41,29 +44,37 @@ class Match {
         // This while loop will keep going as long as both players' health is above 0, which is checked by using the isAlive method for each player
         // While the loop is valid each player will take turn and attack its opponent
         // each attack will reduce the health of the opponent by a random number between 5 and 20, so the minimum number of rounds should be 5
-        while startingPlayer.isAlive() && followingPlayer.isAlive() {
-            
-            var startingPlayerAttack = startingPlayer.attack()
-            followingPlayer.health -= startingPlayerAttack.damage
-            println("1. \(startingPlayer.name) used: \(startingPlayerAttack.message) (-\(startingPlayerAttack.damage) pts)")
+//        while startingPlayer.isAlive() && followingPlayer.isAlive() {
+        
+//            attackRound(attackingPlayer: startingPlayer, defendingPlayer: followingPlayer)
+        
+//            var startingPlayerAttack = startingPlayer.attack()
+//            followingPlayer.health -= startingPlayerAttack.damage
+//            println("1. \(startingPlayer.name) used: \(startingPlayerAttack.message) (-\(startingPlayerAttack.damage) pts)")
             // After the attack of the first player checks whether the second player has enough health to launch an attack
-            if !followingPlayer.isAlive() {
-                break
-            }
-            
-            var followingPlayerAttack = followingPlayer.attack()
-            startingPlayer.health -= followingPlayerAttack.damage
-            println("2. \(followingPlayer.name) used: \(followingPlayerAttack.message) (-\(followingPlayerAttack.damage) pts)")
-        }
+//            if !followingPlayer.isAlive() {
+//                break
+//            }
         
-        if startingPlayer.isAlive() {
-            println("\(startingPlayer.name) wins!! \(startingPlayer.name)'s health is \(startingPlayer.health), whereas \(followingPlayer.name)'s health is \(followingPlayer.health)")
-        } else {
-            println("\(followingPlayer.name) wins!! \(followingPlayer.name)'s health is \(followingPlayer.health), whereas \(startingPlayer.name)'s health is \(startingPlayer.health)")
-        }
-        println(" ")
+//            attackRound(attackingPlayer: followingPlayer, defendingPlayer: startingPlayer)
+//            var followingPlayerAttack = followingPlayer.attack()
+//            startingPlayer.health -= followingPlayerAttack.damage
+//            println("2. \(followingPlayer.name) used: \(followingPlayerAttack.message) (-\(followingPlayerAttack.damage) pts)")
+//        }
         
+//        if startingPlayer.isAlive() {
+//            println("\(startingPlayer.name) wins!! \(startingPlayer.name)'s health is \(startingPlayer.health), whereas \(followingPlayer.name)'s health is \(followingPlayer.health)")
+//        } else {
+//            println("\(followingPlayer.name) wins!! \(followingPlayer.name)'s health is \(followingPlayer.health), whereas \(startingPlayer.name)'s health is \(startingPlayer.health)")
+//        }
+//        println(" ")
         
+    }
+    
+    func attackRound (#attackingPlayer: Player, defendingPlayer: Player)->(attackMessage:String, attackDamage: Int) {
+        var playerAttack = attackingPlayer.attack()
+        
+        return ("\(attackingPlayer.name) used:\n\(playerAttack.message) (-\(playerAttack.damage) pts)", defendingPlayer.health - playerAttack.damage)
     }
     
 }
--- WizardGame/WizardGame/ViewController.swift
+++ WizardGame/WizardGame/ViewController.swift
@@ -19,9 +19,11 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var computerPlayerHealth: UILabel!
     @IBOutlet weak var mainLabel: UITextView!
     
-    // Creating 4 global variables
+    // Creating 5 global variables
     var player1 = Player(playerName: "")
     var player2 = Player(playerName: "")
+    // playerTurn is a variable that indicates what player is currently playing
+    var playerTurn = 0
     
     // *****************************************
     // For some reason I can't instantiate Match() by using the player1 and player2
@@ -49,18 +51,43 @@ class ViewController: UIViewController, UITextFieldDelegate {
             computerPlayerSchool.text = player2.school
             computerPlayerHealth.text = String(player2.health)
             
-            mainLabel.text = "Let the game begin!\n\(player1.name) from \(player1.school) will play against \(player2.name) from \(player2.school)"
+            // We're now starting a new match with player1 against player2
+            newMatch = Match(player1: player1, player2: player2)
             
+            mainLabel.text = "Let the game begin!\n\(player1.name) from \(player1.school) will play against \(player2.name) from \(player2.school)"
+            // Changing readyToPlay to 2 ('game is started')
             readyToPlay = 2
+            // Changing playerTurn to 1 ('1st player to play')
+            playerTurn = 1
+            
             println("readyToPlay has been changed to: \(readyToPlay)")
             
         } else if readyToPlay == 2 {
+
+            if playerTurn == 1 {
+                var attackResults = newMatch.attackRound(attackingPlayer: player1, defendingPlayer: player2)
+                
+                player2.health = attackResults.attackDamage
+                computerPlayerHealth.text = String(player2.health)
+                
+                mainLabel.text = attackResults.attackMessage
+                
+                // Indicates that it is the second player's turn to play on the next round
+                playerTurn = 2
+                
+            } else {
+                var attackResults = newMatch.attackRound(attackingPlayer: player2, defendingPlayer: player1)
+
+                player1.health = attackResults.attackDamage
+                humanPlayerHealth.text = String(player1.health)
+                
+                mainLabel.text = attackResults.attackMessage
+                
+                // Indicates that it is the first player's turn to play on the next round
+                playerTurn = 1
+            }
             
-            computerPlayerHealth.text = String(80)
             
-            mainLabel.text = "Here is the game so far:\n\(player1.name) from \(player1.school) will play against \(player2.name) from \(player2.school)"
-            
-            readyToPlay = 0
             
         } else {
             println("readyToPlay is now: \(readyToPlay)")
