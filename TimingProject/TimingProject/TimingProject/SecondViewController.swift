9af43103b843256aca13015f6cf0ce5b2829d694
Alexis Saint-Jean <asaintjean@gmail.com>
2015-01-31 11:10:44 -0500

    TimingProject: added a textfield, label, and segmented control to secondVC

--- TimingProject/TimingProject/TimingProject/SecondViewController.swift
+++ TimingProject/TimingProject/TimingProject/SecondViewController.swift
@@ -8,10 +8,9 @@
 
 import UIKit
 
-class SecondViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
-    @IBAction func numbersPicked(sender: AnyObject) {
-//        self.pickerView(pickerView: UIPickerView, didSelectRow: Int, inComponent: Int)
-    }
+class SecondViewController: UIViewController, UITextFieldDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
+    @IBOutlet weak var goalName: UITextField!
+
     @IBOutlet weak var picker: UIPickerView!
     
     // I want the minutes component in the UIPicker to look like it is an infinite loop
@@ -22,26 +21,13 @@ class SecondViewController: UIViewController, UIPickerViewDataSource, UIPickerVi
     
     var goalTimeData = [ [" 0"," 1"," 2"," 3"," 4"," 5"," 6"," 7"," 8"," 9", "10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48"],["hours"],["min"]]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.picker.dataSource = self
-        self.picker.delegate = self
 
-        // Here I add multiple copies of 'minutesArray' into the 'lotsOfMinutes' array
-        for i in 0...99 {
-            self.lotsOfMinutes += self.minutesArray
-            println("\(self.lotsOfMinutes.count)")
-        }
-        
-        // I then insert the 'lotsOfMinutes' array at the second-to-last position in 
-        // the 'goaltTimeData' array of arrays
-        self.goalTimeData.insert(self.lotsOfMinutes, atIndex: self.goalTimeData.count-1)
-        
-        // Finally I select a row right in the middle of the minutes array of 'goalTimeData'
-        // So it looks like minutes is an infinite loop
-        self.picker.selectRow(self.goalTimeData[2].count / 2, inComponent: 2, animated: false)
-   
+    
+    // Function that ensures the keyboard disappear after the player enters 'Return'
+    // on the pop-up keyboard when they are done entering their name
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
     
     // The following function is required as part of the
@@ -81,11 +67,36 @@ class SecondViewController: UIViewController, UIPickerViewDataSource, UIPickerVi
         return 35
     }
     
-    
     // Catpure the picker view selection   
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         println("the value from component \(self.goalTimeData[component].count) is \(self.goalTimeData[component][row])")
     }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.goalName.delegate = self
+        
+        self.picker.dataSource = self
+        self.picker.delegate = self
+        
+        // Here I add multiple copies of 'minutesArray' into the 'lotsOfMinutes' array
+        for i in 0...99 {
+            self.lotsOfMinutes += self.minutesArray
+            println("\(self.lotsOfMinutes.count)")
+        }
+        
+        // I then insert the 'lotsOfMinutes' array at the second-to-last position in
+        // the 'goaltTimeData' array of arrays
+        self.goalTimeData.insert(self.lotsOfMinutes, atIndex: self.goalTimeData.count-1)
+        
+        // Finally I select a row right in the middle of the minutes array of 'goalTimeData'
+        // So it looks like minutes is an infinite loop
+        self.picker.selectRow(self.goalTimeData[2].count / 2, inComponent: 2, animated: false)
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
