cc6d0d2e553e98f64f46467fbdeb2db5fa4978d5
Alexis Saint-Jean <asaintjean@gmail.com>
2015-02-08 00:05:52 -0500

    working through the HitList app example for core data found on the Internet

---
+++ HitList/HitList/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Alexis Saint-Jean on 2/7/15.
+//  Copyright (c) 2015 Alexis Saint-Jean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource  {
+    @IBOutlet weak var tableView: UITableView!
+    @IBAction func addName(sender: AnyObject) {
+        var alert = UIAlertController(title: "New name",
+            message: "Add a new name",
+            preferredStyle: .Alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+            style: .Default) { (action: UIAlertAction!) -> Void in
+                
+                let textField = alert.textFields![0] as UITextField
+                self.names.append(textField.text)
+                self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+            style: .Default) { (action: UIAlertAction!) -> Void in
+        }
+        
+        alert.addTextFieldWithConfigurationHandler {
+            (textField: UITextField!) -> Void in
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert,
+            animated: true,
+            completion: nil)
+    }
+    
+    //Insert below the tableView IBOutlet
+    var names = [String]()
+    
+    
+    // MARK: UITableViewDataSource
+    func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            return names.count
+    }
+    
+    func tableView(tableView: UITableView,
+        cellForRowAtIndexPath
+        indexPath: NSIndexPath) -> UITableViewCell {
+            
+            let cell =
+            tableView.dequeueReusableCellWithIdentifier("Cell")
+                as UITableViewCell
+            
+            cell.textLabel!.text = names[indexPath.row]
+            
+            return cell
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Friends List"
+        self.tableView.registerClass(UITableViewCell.self,
+            forCellReuseIdentifier: "Cell")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
