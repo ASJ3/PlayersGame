1b69b2778b7ac998e662a92fc3cb9ab6e1296444
Alexis Saint-Jean <asaintjean@gmail.com>
2015-02-07 19:19:43 -0500

    Created a tableview embedded in a UIViewController instead of a UITableViewController. The "Add new goal" button on the nav bar presents modally the goal setting controller.

--- TimingProject/TimingProject/TimingProject/ViewController.swift
+++ TimingProject/TimingProject/TimingProject/ViewController.swift
@@ -8,28 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
-    @IBOutlet weak var goalNameField: UITextField!
-    @IBOutlet weak var goalHoursField: UITextField!
-    @IBOutlet weak var goalMinutesField: UITextField!
-    @IBOutlet weak var goalDaysField: UITextField!
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var goals = ["Run","Swim", "Learn French", "Learn Programming"]
+    
+    @IBAction func addNewGoal(sender: AnyObject) {
+        let vc = storyboard?.instantiateViewControllerWithIdentifier("secondVC") as SecondViewController
+        self.presentViewController(vc, animated: true, completion: nil)
+    }
+    @IBOutlet weak var tableView: UITableView!
     
     // Function that ensures the keyboard disappear after the player enters 'Return'
     // on the pop-up keyboard when they are done entering their name
-    func textFieldShouldReturn(textField: UITextField!) -> Bool {
-        textField.resignFirstResponder()
-        return true
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return self.goals.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel?.text = self.goals[indexPath.row]
+        // Configure the cell...
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("You selected cell #\(indexPath.row)!")
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "reuseIdentifier")
         // Add self as the delegate to a bunch of textfields
         // to make sure the keyboard disappear when entering "Return"
-        self.goalNameField.delegate = self
-        self.goalHoursField.delegate = self
-        self.goalMinutesField.delegate = self
-        self.goalDaysField.delegate = self
         
     }
     
