8af4caff72e426a021d0cb2f0260a6804fd70da0
Alexis Saint-Jean <asaintjean@gmail.com>
2015-02-10 18:59:22 -0500

    Linked all the buttons/view to the view controller

--- GoalProject/GoalProject/SecondViewController.swift
+++ GoalProject/GoalProject/SecondViewController.swift
@@ -9,7 +9,48 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    @IBOutlet weak var weekDaySelect: UISegmentedControl!
+    @IBOutlet weak var goalName: UITextField!
+    @IBOutlet weak var allWeekBox: UIView!
+    @IBOutlet weak var allWeekCheckbox: UIView!
+    @IBOutlet weak var mondayBox: UIView!
+    @IBOutlet weak var mondayCheckbox: UIView!
+    @IBOutlet weak var tuesdayBox: UIView!
+    @IBOutlet weak var tuesdayCheckbox: UIView!
+    @IBOutlet weak var wednesdayBox: UIView!
+    @IBOutlet weak var wednesdayCheckbox: UIView!
+    @IBOutlet weak var thursdayBox: UIView!
+    @IBOutlet weak var thursdayCheckbox: UIView!
+    @IBOutlet weak var fridayBox: UIView!
+    @IBOutlet weak var fridayCheckbox: UIView!
+    @IBOutlet weak var saturdayBox: UIView!
+    @IBOutlet weak var saturdayCheckbox: UIView!
+    @IBOutlet weak var sundayBox: UIView!
+    @IBOutlet weak var sundayCheckbox: UIView!
 
+    @IBOutlet weak var hoursAndMinutesPicker: UIPickerView!
+    @IBOutlet weak var picker: UIPickerView!
+    @IBAction func saveButton(sender: AnyObject) {
+    }
+    @IBAction func cancelButton(sender: AnyObject) {
+    }
+
+    @IBAction func sundayTap(sender: UITapGestureRecognizer) {
+    }
+    @IBAction func saturdayTap(sender: UITapGestureRecognizer) {
+    }
+    @IBAction func fridayTap(sender: UITapGestureRecognizer) {
+    }
+    @IBAction func thursdayTap(sender: UITapGestureRecognizer) {
+    }
+    @IBAction func wednesdayTap(sender: UITapGestureRecognizer) {
+    }
+    @IBAction func tuesdayTap(sender: UITapGestureRecognizer) {
+    }
+    @IBAction func mondayTap(sender: UITapGestureRecognizer) {
+    }
+    @IBAction func allWeekTap(sender: UITapGestureRecognizer) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
