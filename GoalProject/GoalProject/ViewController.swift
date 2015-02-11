a579390338aafc950f62b50a449b8673b61e9e7c
Alexis Saint-Jean <asaintjean@gmail.com>
2015-02-10 21:22:12 -0500

    The view controller with the table view is now working and the add button on the nav bar modally presents the second viewcontroller

--- GoalProject/GoalProject/ViewController.swift
+++ GoalProject/GoalProject/ViewController.swift
@@ -8,18 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var goals = ["Run","Swim", "Learn French", "Learn Programming"]
+    
+    @IBAction func addNewGoal(sender: AnyObject) {
+        let vc = storyboard?.instantiateViewControllerWithIdentifier("secondVC") as SecondViewController
+        self.presentViewController(vc, animated: true, completion: nil)
+    }
+    @IBOutlet weak var tableView: UITableView!
+    
+    // Function that ensures the keyboard disappear after the player enters 'Return'
+    // on the pop-up keyboard when they are done entering their name
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return self.goals.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel?.text = self.goals[indexPath.row]
+        // Configure the cell...
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("You selected cell #\(indexPath.row)!")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "reuseIdentifier")
+        // Add self as the delegate to a bunch of textfields
+        // to make sure the keyboard disappear when entering "Return"
+        
     }
-
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
