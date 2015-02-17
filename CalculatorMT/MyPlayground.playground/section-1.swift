200446732a9d9eeee90718c21e74431ecf098181
Alexis Saint-Jean <asaintjean@gmail.com>
2015-02-17 18:58:23 -0500

    More changes to calculating with percentage

--- CalculatorMT/MyPlayground.playground/section-1.swift
+++ CalculatorMT/MyPlayground.playground/section-1.swift
@@ -1,6 +1,7 @@
 // Playground - noun: a place where people can play
 
 import UIKit
+import Foundation
 
 var str = "Hello, playground"
 
@@ -20,4 +21,27 @@ newFloat /= 1000000
 var curFloat = "10000000000000000"
 var newInt = curFloat.toInt()
 
+var numstring: String
+numstring = "12345.9999"
 
+//var convertedNum = numstring.floatValue
+//var endingDigits = currentDisplay.substringWithRange(Range<String.Index>(start: advance(currentDisplay.endIndex, -2), end: currentDisplay.endIndex))
+var leftNumberPart = numstring.substringWithRange(Range<String.Index>(start: numstring.startIndex, end: advance(numstring.startIndex, 5)))
+var rightNumberPart = numstring.substringWithRange(Range<String.Index>(start: advance(numstring.startIndex, 5+1), end: numstring.endIndex))
+var newRightNum = pow(10.0, Double(4))
+
+var result = Double(rightNumberPart.toInt()!) / newRightNum
+
+var digits = "1234.5789"
+
+let characterToFind: Character = "."
+
+var characterPosition = 0
+
+if let characterIndex = find(digits, characterToFind) {
+    characterPosition = distance(digits.startIndex, characterIndex) // -> 7
+} else {
+    "'\(characterToFind)' not found"
+}
+
+var newRes = characterPosition + 1
