0f239ddc436aa6757b8f08a033bc874eb773036f
Alexis Saint-Jean <asaintjean@gmail.com>
2015-02-22 22:28:45 -0500

    transferred all the calculations to a Calculations Class. Fixed the equalButton code in the ViewController so that the app doesn't crash anymore if the user taps on "=" multiple times (the result is just the number already shown on screen, so the user has the impression that nothing changes)

--- CalculatorMT/CalculatorMT/ViewController.swift
+++ CalculatorMT/CalculatorMT/ViewController.swift
@@ -9,18 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
-    // The decimalAdded variable is a boolean to check whether
-    // the user already added decimals
-    var decimalAdded = false
+    /********** PLEASE NOTE **********
+    **********************************
+    
+    Certain keys on the calculator do not work (didn't finish the code for them yet)
+    - the 'back arrow' key: the purpose of this key is to delete just one digit at a time, in case the user made a typo in entering the number
+    - the percent key: if the user manually enter a number and divides it by 100, the calculator still gives the right result. I just haven't had time to create the code for that key yet.
+    - Dividing by 0 only returns 0 for now, instead of an 'Error' message
+    - although the user cannot enter a number > 999,999,999 (number limit similar to Apple's calculator app), the results shown can be greater than this.
+    - Everything else should work properly
+    
+    **********************************
+    **********************************/
+    
     
     var numberOfKeystrokes = 0
     
+    // CalcNumber() objects created to store the values entered and change them into Double or String depending on whether we need to calculate with them or just display them on screen
     var currentCalcNumber = CalcNumber()
     var firstCalcNumber = CalcNumber()
     var secondCalcNumber = CalcNumber()
     
+    // operationsNumber is an array storing 2 CalcNumber objects to perform a caculation on them
     var operationNumbers = [CalcNumber]()
     
+    // Booleans to check whether we are entering the values for the first or the second number
     var firstNumberEntered = false
     var secondNumberEntered = false
     var startingOtherNumber = false
@@ -88,17 +101,19 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBAction func equalButton(sender: AnyObject) {
         var result = 0.0
         var resultCalcNumber = CalcNumber()
+        self.operationNumbers.append(self.currentCalcNumber)
+        if self.operationNumbers.count == 1 {
+            self.operationNumbers.append(resultCalcNumber)
+            self.calcOperation = "+"
+        }
+        var calculationResult = Calculations(ArrayOfNumbers: self.operationNumbers)
         
-            self.operationNumbers.append(self.currentCalcNumber)
-
-        if self.calcOperation == "+" {
-            result = self.operationNumbers[0].turnIntoDouble() + self.operationNumbers[1].turnIntoDouble()
+        if self.calcOperation != "" {
+            result = calculationResult.operate(self.calcOperation)
             resultCalcNumber.initWithDouble(result)
             self.operationNumbers = []
-            println("operationsNumbers count: \(self.operationNumbers.count)")
-
+            self.calcOperation = ""
         }
-        
         self.currentCalcNumber = resultCalcNumber
         self.numberField.text = self.currentCalcNumber.turnIntoFormattedString()
     }
@@ -109,12 +124,18 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
 
     @IBAction func minusButton(sender: AnyObject) {
+        self.calcOperation = "-"
+        calcDisplayManager("operation")
     }
     
     @IBAction func multiplyButton(sender: AnyObject) {
+        self.calcOperation = "x"
+        calcDisplayManager("operation")
     }
     
     @IBAction func divideButton(sender: AnyObject) {
+        self.calcOperation = "/"
+        calcDisplayManager("operation")
     }
     
 
