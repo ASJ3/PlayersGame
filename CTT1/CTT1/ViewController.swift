3619379a71398d7d6be6cbac2b2a2602a6b24f96
Alexis Saint-Jean <asaintjean@gmail.com>
2015-03-14 11:08:32 -0400

    CTT: latest changes to the quoteVC layout

--- CTT1/CTT1/ViewController.swift
+++ CTT1/CTT1/ViewController.swift
@@ -11,16 +11,16 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     
-    var midtempData = [["Quote": "I see God’s hand in everything around us including the whole universe. If it suited His purposes not just to have one planet that could sustain life that would give rise to intelligence, fine. I don't see any reason to be shaken or object to that at all", "Author": "Francis Collins", "Profession": "Geneticist"],
-        ["Quote": "The big bang is not a point in space. It’s a moment in time. It’s a moment when the density of the universe was infinite.", "Author": "Sean Carroll", "Profession": "Physicist"],
-        ["Quote": "One of the most important things which our minds undertake is to understand other human beings. We’ve become - we’ve evolved to be - what I call ‘natural psychologists’, who are brilliant at mind reading.", "Author": "Nicholas Humphrey", "Profession": "Psychologist"],
-        ["Quote": "Is it possible that this idea of God is something more than merely a functional idea. Could it be that under this world as we find it, there is some sort of deeper reality.", "Author": "Philip Clayton", "Profession": "Philosopher"],
-        ["Quote": "It is remarkable that the complexity of our world can be explained in terms of simple physical laws and that these laws, which we can study in a lab, apply in the remotest galaxies.", "Author": "Martin Rees", "Profession": "Astronomer"],
-        ["Quote": "I see God’s hand in everything around us including the whole universe. If it suited His purposes not just to have one planet that could sustain life that would give rise to intelligence, fine. I don't see any reason to be shaken or object to that at all", "Author": "Francis Collins", "Profession": "Geneticist"]]
-//        ["Quote": "The big bang is not a point in space. It’s a moment in time. It’s a moment when the density of the universe was infinite.", "Author": "Sean Carroll", "Profession": "Physicist"],
-//        ["Quote": "One of the most important things which our minds undertake is to understand other human beings. We’ve become - we’ve evolved to be - what I call ‘natural psychologists’, who are brilliant at mind reading.", "Author": "Nicholas Humphrey", "Profession": "Psychologist"],
-//        ["Quote": "Is it possible that this idea of God is something more than merely a functional idea. Could it be that under this world as we find it, there is some sort of deeper reality.", "Author": "Philip Clayton", "Profession": "Philosopher"],
-//        ["Quote": "It is remarkable that the complexity of our world can be explained in terms of simple physical laws and that these laws, which we can study in a lab, apply in the remotest galaxies.", "Author": "Martin Rees", "Profession": "Astronomer"]]
+    var midtempData = [["quote": "I see God’s hand in everything around us including the whole universe. If it suited His purposes not just to have one planet that could sustain life that would give rise to intelligence, fine. I don't see any reason to be shaken or object to that at all", "contributor_name": "Francis Collins", "Profession": "Geneticist"],
+        ["quote": "The big bang is not a point in space. It’s a moment in time. It’s a moment when the density of the universe was infinite.", "contributor_name": "Sean Carroll", "Profession": "Physicist"],
+        ["quote": "One of the most important things which our minds undertake is to understand other human beings. We’ve become - we’ve evolved to be - what I call ‘natural psychologists’, who are brilliant at mind reading.", "contributor_name": "Nicholas Humphrey", "Profession": "Psychologist"],
+        ["quote": "Is it possible that this idea of God is something more than merely a functional idea. Could it be that under this world as we find it, there is some sort of deeper reality.", "contributor_name": "Philip Clayton", "Profession": "Philosopher"],
+        ["quote": "It is remarkable that the complexity of our world can be explained in terms of simple physical laws and that these laws, which we can study in a lab, apply in the remotest galaxies.", "Author": "Martin Rees", "Profession": "Astronomer"],
+        ["quote": "I see God’s hand in everything around us including the whole universe. If it suited His purposes not just to have one planet that could sustain life that would give rise to intelligence, fine. I don't see any reason to be shaken or object to that at all", "contributor_name": "Francis Collins", "Profession": "Geneticist"]]
+//        ["quote": "The big bang is not a point in space. It’s a moment in time. It’s a moment when the density of the universe was infinite.", "contributor_name": "Sean Carroll", "Profession": "Physicist"],
+//        ["quote": "One of the most important things which our minds undertake is to understand other human beings. We’ve become - we’ve evolved to be - what I call ‘natural psychologists’, who are brilliant at mind reading.", "contributor_name": "Nicholas Humphrey", "Profession": "Psychologist"],
+//        ["quote": "Is it possible that this idea of God is something more than merely a functional idea. Could it be that under this world as we find it, there is some sort of deeper reality.", "contributor_name": "Philip Clayton", "Profession": "Philosopher"],
+//        ["quote": "It is remarkable that the complexity of our world can be explained in terms of simple physical laws and that these laws, which we can study in a lab, apply in the remotest galaxies.", "contributor_name": "Martin Rees", "Profession": "Astronomer"]]
     
     var oldtempData = ["I see God’s hand in everything around us including the whole universe. If it suited His purposes not just to have one planet that could sustain life that would give rise to intelligence, fine. I don’t see any reason to be shaken or object to that at all.",
         "The big bang is not a point in space. It’s a moment in time. It’s a moment when the density of the universe was infinite.",
@@ -51,7 +51,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
         
-            cell.textLabel?.text = midtempData[indexPath.row]["Quote"]
+            cell.textLabel?.text = midtempData[indexPath.row]["quote"]
         
         return cell
     }
@@ -73,14 +73,14 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         if let forsure = segue.destinationViewController as? quoteViewController {
             
             // **********
-            // I'm having issues with setting the text variable to the "Quote" of the sender (is it because it is an AnyObject?) 
+            // I'm having issues with setting the text variable to the "quote" of the sender (is it because it is an AnyObject?)
             // So first I am setting a local variable, localdictionary, then I am assigning the sender to it
-            var localdictionary = ["Quote": "", "Author": "", "Profession": ""]
+            var localdictionary = ["quote": "", "contributor_name": "", "Profession": ""]
             localdictionary = sender as Dictionary
-//            var text = sender["Quote"] as? NSString
-            var text = localdictionary["Quote"]
+//            var text = sender["quote"] as? NSString
+            var text = localdictionary["quote"]
             forsure.textOfQuote = text
-            var authorName = localdictionary["Author"]
+            var authorName = localdictionary["contributor_name"]
             forsure.authorOfQuote = authorName
             var profession = localdictionary["Profession"]
             forsure.professionOfAuthor = profession
