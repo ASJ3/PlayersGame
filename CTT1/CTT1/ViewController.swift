426f3246e71cc16fad63e70a3a0d1398494d519c
Alexis Saint-Jean <asaintjean@gmail.com>
2015-03-16 17:45:31 -0400

    Starting to integrate the json code into the view controller with the tableView

--- CTT1/CTT1/ViewController.swift
+++ CTT1/CTT1/ViewController.swift
@@ -9,8 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
     
     var firstTimeLoading = true
+    var json: NSArray?
 
     
     var midtempData = [["quote": "I see God’s hand in everything around us including the whole universe. If it suited His purposes not just to have one planet that could sustain life that would give rise to intelligence, fine. I don't see any reason to be shaken or object to that at all", "contributor_name": "Francis Collins", "Profession": "Geneticist"],
@@ -19,26 +21,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         ["quote": "Is it possible that this idea of God is something more than merely a functional idea. Could it be that under this world as we find it, there is some sort of deeper reality.", "contributor_name": "Philip Clayton", "Profession": "Philosopher"],
         ["quote": "It is remarkable that the complexity of our world can be explained in terms of simple physical laws and that these laws, which we can study in a lab, apply in the remotest galaxies.", "contributor_name": "Martin Rees", "Profession": "Astronomer"],
         ["quote": "I see God’s hand in everything around us including the whole universe. If it suited His purposes not just to have one planet that could sustain life that would give rise to intelligence, fine. I don't see any reason to be shaken or object to that at all", "contributor_name": "Francis Collins", "Profession": "Geneticist"]]
-//        ["quote": "The big bang is not a point in space. It’s a moment in time. It’s a moment when the density of the universe was infinite.", "contributor_name": "Sean Carroll", "Profession": "Physicist"],
-//        ["quote": "One of the most important things which our minds undertake is to understand other human beings. We’ve become - we’ve evolved to be - what I call ‘natural psychologists’, who are brilliant at mind reading.", "contributor_name": "Nicholas Humphrey", "Profession": "Psychologist"],
-//        ["quote": "Is it possible that this idea of God is something more than merely a functional idea. Could it be that under this world as we find it, there is some sort of deeper reality.", "contributor_name": "Philip Clayton", "Profession": "Philosopher"],
-//        ["quote": "It is remarkable that the complexity of our world can be explained in terms of simple physical laws and that these laws, which we can study in a lab, apply in the remotest galaxies.", "contributor_name": "Martin Rees", "Profession": "Astronomer"]]
-    
-    var oldtempData = ["I see God’s hand in everything around us including the whole universe. If it suited His purposes not just to have one planet that could sustain life that would give rise to intelligence, fine. I don’t see any reason to be shaken or object to that at all.",
-        "The big bang is not a point in space. It’s a moment in time. It’s a moment when the density of the universe was infinite.",
-        "One of the most important things which our minds undertake is to understand other human beings. We’ve become - we’ve evolved to be - what I call ‘natural psychologists’, who are brilliant at mind reading.",
-        "Is it possible that this idea of God is something more than merely a functional idea. Could it be that under this world as we find it, there is some sort of deeper reality.",
-        "It is remarkable that the complexity of our world can be explained in terms of simple physical laws and that these laws, which we can study in a lab, apply in the remotest galaxies.",
-        "Quote 6",
-        "Quote 7",
-        "Quote 8",
-        "Quote 9",
-        "Quote 10",
-        "Quote 11",
-        "Quote 12",
-        "Quote 13",
-        "Quote 14",
-        "Quote 15", "Quote 16", "Quote 17", "Quote 18","Quote 19", "Quote 20"]
+
     
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -47,7 +30,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //For now uses the tempData array to create the number of cells needed
-        return midtempData.count
+//        return midtempData.count
+        if let jsonData = self.json {
+            println("Number of articles in the json file is: \(self.json!.count)")
+            return self.json!.count
+        }
+        println("jsonData hasn't loaded yet")
+        return 0
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -95,6 +84,23 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        println("beginning of viewDidLoad")
+        
+        // working on loading JSON
+        if let url = NSURL(string: "https://raw.githubusercontent.com/ASJ3/PlayersGame/master/API_JSON/all-quotes.json") {
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
+                if let jsonDict: AnyObject = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) {
+                    self.json = jsonDict as? NSArray
+                    println("json count is now \(self.json!.count)")
+                }
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    // IMPORTANT we need to reload the data we got into our table view
+//                    self.tableView.reloadData()
+                })
+            })
+            task.resume()
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
