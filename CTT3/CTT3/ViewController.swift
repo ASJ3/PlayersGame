ef606fd5fbe70b7bde8db952e305e2340089d13e
Alexis Saint-Jean <asaintjean@gmail.com>
2015-03-19 00:06:20 -0400

    CTT3: starting to add the QuoteListViewController to the main VC

--- CTT3/CTT3/ViewController.swift
+++ CTT3/CTT3/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var listController: QuoteListViewController?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        self.listController = QuoteListViewController()
     }
 
     override func didReceiveMemoryWarning() {
