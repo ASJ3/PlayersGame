a27fab944c97a68f91b2ecdf429741f055f61fff
Alexis Saint-Jean <asaintjean@gmail.com>
2015-03-18 21:23:38 -0400

    CTT3 beginning

---
+++ CTT3/CTT3/QuoteListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  QuoteListViewController.swift
+//  CTT3
+//
+//  Created by Alexis Saint-Jean on 3/18/15.
+//  Copyright (c) 2015 Alexis Saint-Jean. All rights reserved.
+//
+
+import UIKit
+
+class QuoteListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
