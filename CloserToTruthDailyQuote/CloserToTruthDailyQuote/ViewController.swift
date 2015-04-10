01c864c96cfba94b21042392aee5414a623bcddc
Alexis Saint-Jean <asaintjean@gmail.com>
2015-04-09 22:05:37 -0400

    Created a new Blank app called CloserToTruthDailyQuote to learn more about using cocoapods and using the Kaltura SDK

---
+++ CloserToTruthDailyQuote/CloserToTruthDailyQuote/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  CloserToTruthDailyQuote
+//
+//  Created by Alexis Saint-Jean on 4/9/15.
+//  Copyright (c) 2015 Alexis Saint-Jean. All rights reserved.
+//
+
+import UIKit
+import pop
+import snap
+
+class ViewController: UIViewController {
+    
+    func showMenu() {
+        let toggleMenuIn = POPSpringAnimation(propertyNamed: kPOPLayoutConstraintConstant)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
