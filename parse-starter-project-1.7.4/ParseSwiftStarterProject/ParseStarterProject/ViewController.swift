91868779eda5a4d56951992ac79e757bb610957a
Alexis Saint-Jean <asaintjean@gmail.com>
2015-06-13 15:47:38 -0400

    Resilient: Created user interface to enter information with a submit button at the bottom

--- parse-starter-project-1.7.4/ParseSwiftStarterProject/ParseStarterProject/ViewController.swift
+++ parse-starter-project-1.7.4/ParseSwiftStarterProject/ParseStarterProject/ViewController.swift
@@ -7,38 +7,16 @@
 import UIKit
 import Parse
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-//        let testObject = PFObject(className: "TestObject")
-//        testObject["foo"] = "bar"
-//        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
-//            println("Object has been saved.")
-//        }
-        
-       /* var whisperP = PFObject(className: "Whisper")
-        whisperP["whisper"] = self.whisper.text
-        whisperP["user"] = "Tedi"
-        whisperP["category"] = self.whisperCat.text
-        whisperP.saveInBackgroundWithBlock { (success: Bool, error: NSError!) -> Void in
-            if(success) {
-                println(whisperP.objectId)
-                println("Whisper saved succesfully")
-                self.whisper.text = ""
-                self.whisperCat.text = ""
-            } else {
-                println(error.description)
-            }
-        }
-*/
+class ViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var gender: UITextField!
+    @IBOutlet weak var personalStory: UITextView!
+    @IBAction func submitInfo(sender: AnyObject) {
         
         var whisperP = PFObject(className: "Whisper")
-        whisperP["whisper"] = "Hello @ World!"
-        whisperP["user"] = "Sylvia"
-        whisperP["category"] = "Female"
+        whisperP["whisper"] = self.personalStory.text
+        whisperP["user"] = self.firstName.text
+        whisperP["category"] = self.gender.text
         whisperP.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
             if(success) {
                 println(whisperP.objectId)
@@ -47,10 +25,37 @@ class ViewController: UIViewController {
                 println(error!.description)
             }
         }
-
-        
         
     }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        let testObject = PFObject(className: "TestObject")
+//        testObject["foo"] = "bar"
+//        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+//            println("Object has been saved.")
+//        }
+        
+        self.firstName.delegate = self;
+        self.gender.delegate = self;
+        self.personalStory.delegate = self;
+    }
+    
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        if(text == "\n") {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
