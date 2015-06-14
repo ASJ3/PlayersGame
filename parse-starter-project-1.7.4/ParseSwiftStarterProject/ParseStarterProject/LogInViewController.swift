aaf7dc2912eb7ea0b66a0ecdc40b149370590530
Alexis Saint-Jean <asaintjean@gmail.com>
2015-06-13 20:20:12 -0400

    Resilient: Finished logic to check whether a user already exists. Also created a button and the logic to register a new user.

--- parse-starter-project-1.7.4/ParseSwiftStarterProject/ParseStarterProject/LogInViewController.swift
+++ parse-starter-project-1.7.4/ParseSwiftStarterProject/ParseStarterProject/LogInViewController.swift
@@ -7,26 +7,62 @@
 //
 
 import UIKit
+import Parse
 
 class LogInViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var userName: UITextField!
     @IBOutlet weak var userPassword: UITextField!
     @IBAction func loggingIn(sender: AnyObject) {
-        if self.userName.text == "Alexis" {
-            println("Correct user name")
-            self.performSegueWithIdentifier("showFirstUseScreen", sender: nil)
-        } else {
-            println("Incorrect user name")
+        
+        PFUser.logInWithUsernameInBackground(self.userName.text, password: self.userPassword.text) { (user, error) -> Void in
+                if user != nil {
+                    println("Log in successful")
+                    self.performSegueWithIdentifier("showFirstUseScreen", sender: nil)
+                } else {
+                    println("Log in Error")
+                }
+        }
+//        if self.userName.text == "Alexis" {
+//            println("Correct user name")
+//            
+//        } else {
+//            println("Incorrect user name")
+//        }
+    }
+    
+    @IBAction func Register(sender: AnyObject) {
+        var user = PFUser()
+        user.username = self.userName.text
+        user.password = self.userPassword.text
+        user.email = self.userName.text
+        
+        user.signUpInBackgroundWithBlock { (succeeded, error) -> Void in
+            if error == nil {
+                self.userName.text = ""
+                self.userPassword.text = ""
+                self.performSegueWithIdentifier("showFirstUseScreen", sender: nil)
+                println("Registration successful")
+            } else {
+                println("Couldn't register")
+                println("\(error)")
+            }
         }
     }
-
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.userName.delegate = self
         self.userPassword.delegate = self
-  
+        
+        var currentUser = PFUser.currentUser()
+        if currentUser != nil {
+            println("you are logged in!")
+            println(currentUser)
+        } else  {
+            println("Not logged in!")
+        }
     }
 
     func textFieldShouldReturn(textField: UITextField) -> Bool {
