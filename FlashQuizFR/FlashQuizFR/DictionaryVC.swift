51216cfb80cacd96f7787c1008826183cf2cc635
Alexis Saint-Jean <asaintjean@gmail.com>
2015-07-18 18:50:16 -0400

    FlashQuizFR: dictionaryVC table now using the plist.

--- FlashQuizFR/FlashQuizFR/DictionaryVC.swift
+++ FlashQuizFR/FlashQuizFR/DictionaryVC.swift
@@ -13,12 +13,20 @@ class DictionaryVC: UIViewController, UITableViewDelegate, UITableViewDataSource
     @IBOutlet weak var table: UITableView!
     //TRIAL: creating an array of words for the table content
     var wordArray = ["avion","bateau","camion","train","ferry","helicoptere"]
+    var wordListArray = NSMutableArray()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+
+//        println("DictionaryVC: The array of words is: \(self.wordArray)")
+        
+        
+        //TRIAL: Loading all words and their translation from the defaultWordList plist
+        var wordListPath = NSBundle.mainBundle().pathForResource("defaultWordList", ofType: "plist")
+        wordListArray = NSMutableArray(contentsOfFile: wordListPath!)!
+        
         self.table.reloadData()
-        println("DictionaryVC: The array of words is: \(self.wordArray)")
     }
     
     
@@ -27,7 +35,7 @@ class DictionaryVC: UIViewController, UITableViewDelegate, UITableViewDataSource
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.wordArray.count
+        return self.wordListArray.count
     }
     
     
@@ -39,7 +47,7 @@ class DictionaryVC: UIViewController, UITableViewDelegate, UITableViewDataSource
         }
         cell.textLabel?.textColor = UIColor.blackColor()
         cell.backgroundColor = UIColor.clearColor()
-        cell.textLabel?.text = wordArray[indexPath.row]
+        cell.textLabel?.text = wordListArray[indexPath.row] as? String
         return cell
     }
     
