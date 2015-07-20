79c260f9cf1358bba94b90b039786f0d5ddfee9d
Alexis Saint-Jean <asaintjean@gmail.com>
2015-07-20 08:00:20 -0400

    FlashQuizFR: count the number times each letter appears as the first letter (to help us create letter sections in the tableView of dictionaryVC).

--- FlashQuizFR/FlashQuizFR/DictionaryVC.swift
+++ FlashQuizFR/FlashQuizFR/DictionaryVC.swift
@@ -18,9 +18,21 @@ class DictionaryVC: UIViewController, UITableViewDelegate, UITableViewDataSource
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //TRIAL: Loading all words and their translation from the defaultWordList plist
+        //TRIAL: Loading all words and their translation from the wordDictionary plist
         var wordListPath = NSBundle.mainBundle().pathForResource("wordDictionary", ofType: "plist")
         wordListArray = NSMutableArray(contentsOfFile: wordListPath!)!
+        
+        //TRIAL: try to get the number of sections in English based on the number of unique first letters we have in wordDictionary
+        var firstLetterArray: [String:Int] = ["a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h":0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0]
+
+        //Increase the count of the values in firsLetterArray based on the number of times each letter appears as the first letter in each word of wordDictionary
+        for word in wordListArray {
+            var firstChar = word["wordFirst"] as! String
+            firstLetterArray[firstChar]! += 1
+            
+            }
+        
+        println("DictionaryVC: \(firstLetterArray)")
 
         
         self.table.reloadData()
@@ -46,8 +58,7 @@ class DictionaryVC: UIViewController, UITableViewDelegate, UITableViewDataSource
         cell.backgroundColor = UIColor.clearColor()
         
         //Note: we are using the objectForKey because the plist is an array of dictionaries, and we want
-        //to retrieve certain parts of these dictionaries. However, since we dictionaryVC does not know
-        //about the structure of the dictionaries in the plist, we cannot use dot notation.
+        //to retrieve certain parts of these dictionaries, and objectForKey is the way to to this for dictionaries in plists.
         cell.textLabel?.text = wordListArray[indexPath.row].objectForKey("word") as? String
         var wordGender = wordListArray[indexPath.row].objectForKey("gender") as! String
         cell.detailTextLabel?.text = wordListArray[indexPath.row].objectForKey("translation") as! String + " (" + wordGender + ")"
---
+++ FlashQuizPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var firstLetterArray = [["a": 0], ["b": 0], ["c": 0],["r": 0],["v": 0]]
+//firstLetterArray["a"]
+var secLetterArray = ["a": 0, "b": 0, "c": 0,"r": 0,"v": 0]
+var secLetter = secLetterArray["a"]
+secLetter! += 1
+secLetter
+secLetterArray
+secLetterArray["a"] = secLetter
+secLetterArray
+secLetterArray["b"] = 3
+secLetterArray
+
