d27a432015166914e571a7507f72058dd61744d9
Alexis Saint-Jean <asaintjean@gmail.com>
2015-07-25 18:58:19 -0400

    FlashQuizFR: everything ready for Sections in CoreData tableview. Saving before flipping the switch.

--- FlashQuizFR/FlashQuizFR/TestVC.swift
+++ FlashQuizFR/FlashQuizFR/TestVC.swift
@@ -15,8 +15,10 @@ class TestVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     var words = [NSManagedObject]()
     var nativeFirstArray = [String]()
-    var sortedNativeFirstArray = [AnyObject]()
-    var NativeWordList = [String: [AnyObject]]()
+//    var sortedNativeFirstArray = [AnyObject]()
+    var uniqueNativeFirstArray = [String]()
+    var nativeWordList = [String: [AnyObject]]()
+    var sortedNativeWordList = [AnyObject]()
     var wordFromList = ["word":String(),  "wordFirst":String(), "translation":String(), "translationFirst":String(), "gender":String()]
 
 
@@ -81,52 +83,45 @@ class TestVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
                 self.nativeFirstArray.append(nativeFirst!)
             }
             
-            //Sort the letters in nativeFirstArray in alphabetical order
-            self.nativeFirstArray.sort(){$0 <  $1}
+            
             //Create a sorted array listing each unique letter
-            var uniqueNativeFirstArray = Array(Set(self.nativeFirstArray))
+            uniqueNativeFirstArray = Array(Set(self.nativeFirstArray))
             uniqueNativeFirstArray.sort(){$0 <  $1}
-            //Create an array listing the occurences of each letter in uniqueNativeFirstArray
-            var uniqueNativeFirstCountArray = [Int]()
-            
-            for i in uniqueNativeFirstArray {
-                var countOfLetterOccurences = 0
-                for j in self.nativeFirstArray {
-                    if j == i {
-                    countOfLetterOccurences += 1
-                    }
-                }
-                uniqueNativeFirstCountArray.append(countOfLetterOccurences)
-            }
-            
             println("TestVC: uniqueNativeFirstArray is : \(uniqueNativeFirstArray)")
-            println("TestVC: uniqueNativeFirstCountArray is : \(uniqueNativeFirstCountArray)")
+
             
             //Add dictionaries to sorterNativeFirstArray that indicates each letter and how many words start by that letter 
-            for i in 0...uniqueNativeFirstArray.count-1 {
-                var dictKey = uniqueNativeFirstArray[i]
-//                var dictLetterWordArray = []
-//                var nativeFirstDict = [dictKey: dictCount]
-//                self.sortedNativeFirstArray.append(nativeFirstDict)
+            for firstLetter in uniqueNativeFirstArray {
+                
                 for word in words {
-                    var nativeFirst = word.valueForKey("wordFirst") as? String
-                    if nativeFirst! == dictKey {
+                    if firstLetter == word.valueForKey("wordFirst") as! String {
                         wordFromList["word"] = word.valueForKey("word") as? String
                         wordFromList["wordFirst"] = word.valueForKey("wordFirst") as? String
                         wordFromList["translation"] = word.valueForKey("translation") as? String
                         wordFromList["translationFirst"] = word.valueForKey("translationFirst") as? String
                         wordFromList["gender"] = word.valueForKey("gender") as? String
                         
-                        if self.NativeWordList[dictKey] == nil {
-                            self.NativeWordList[dictKey] = [wordFromList]
+                        if self.nativeWordList[firstLetter] == nil {
+                            self.nativeWordList[firstLetter] = [wordFromList]
                         } else {
-                            self.NativeWordList[dictKey]!.append(wordFromList)
+                            self.nativeWordList[firstLetter]!.append(wordFromList)
                         }
                         
                     }
                 }
             }
-            println("TestVC: NativeWordlist is: \(self.NativeWordList)")
+            
+            //Create the final list to use, sortedNativeWordList
+            for i in uniqueNativeFirstArray {
+                for (key, value) in self.nativeWordList {
+                    if key == i {
+                        self.sortedNativeWordList.append(value)
+                        break
+                    }
+                }
+            }
+            
+            println("TestVC: sortedNativeWordlist is: \(self.sortedNativeWordList)")
 
         } else {
             println("Could not fetch \(error), \(error!.userInfo)")
