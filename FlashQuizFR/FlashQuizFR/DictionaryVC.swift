7ab220312d1c85311d8b781368f5299f4afbfddc
Alexis Saint-Jean <asaintjean@gmail.com>
2015-07-27 08:12:34 -0400

    FlashQuizFR: Starting to clean up code in DictionaryVC

--- FlashQuizFR/FlashQuizFR/DictionaryVC.swift
+++ FlashQuizFR/FlashQuizFR/DictionaryVC.swift
@@ -29,27 +29,32 @@ class DictionaryVC: UIViewController, UITableViewDelegate, UITableViewDataSource
         // Do any additional setup after loading the view.
     }
     
+    
+    //The following 5 functions are to set up the table
+    //1: Indicate the number of sections in the table (one section for each letter)
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return self.sortedNativeWordList.count
     }
     
+    //2: Indicate the title of each section (i.e. the letter by which each word in section begins)
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        var sectionLetterHeader = self.sortedNativeWordList[section][0].objectForKey("wordFirst") as? String
+        return sectionLetterHeader?.uppercaseString
+    }
+    
+    //3: Indicate the number of rows (i.e. words) in each section, based on CoreData
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.sortedNativeWordList[section].count
     }
     
+    //4: Indicate the values for the title/subtitle of each cell in the table
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("wordCell") as! UITableViewCell
         let person: AnyObject! = self.sortedNativeWordList[indexPath.section][indexPath.row]
         
         var wordGender = person.valueForKey("gender") as! String
-        var colorOfCell = UIColor()
-        if wordGender == "f" {
-            colorOfCell = UIColor(red: 250.0/255.0, green: 230.0/255.0, blue: 235.0/255.0, alpha: 1.0)
-        } else if wordGender == "m" {
-            colorOfCell = UIColor(red: 229.0/255.0, green: 240.0/255.0, blue: 248.0/255.0, alpha: 1.0)
-        } else {
-            colorOfCell = UIColor.clearColor()
-        }
+        var colorOfCell = cellColorPicker(wordGender)
+        
         cell.backgroundColor = colorOfCell
         cell.textLabel!.text = person.valueForKey("word") as? String
         cell.detailTextLabel?.text = person.valueForKey("translation") as! String + " (" + wordGender + ")"
@@ -58,13 +63,26 @@ class DictionaryVC: UIViewController, UITableViewDelegate, UITableViewDataSource
         
     }
     
+    //5: Indicate the height of each cell in table
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 45
     }
     
-    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        var sectionLetterHeader = self.sortedNativeWordList[section][0].objectForKey("wordFirst") as? String
-        return sectionLetterHeader?.uppercaseString
+    
+    //cellColorPicker() indicate what background color a given cell in the table should have
+    //based on a property of the word in that cell (i.e. blue/pink if the word is masculine/feminine
+    func cellColorPicker(wordInfo: String) -> UIColor {
+        var colorToReturn = UIColor()
+        
+        if wordInfo == "f" {
+            colorToReturn = UIColor(red: 250.0/255.0, green: 230.0/255.0, blue: 235.0/255.0, alpha: 1.0)
+        } else if wordInfo == "m" {
+            colorToReturn = UIColor(red: 229.0/255.0, green: 240.0/255.0, blue: 248.0/255.0, alpha: 1.0)
+        } else {
+            colorToReturn = UIColor.clearColor()
+        }
+        
+        return colorToReturn
     }
     
     
