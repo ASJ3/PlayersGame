539e6454fe84fde2f5f6d5952005f9072049b9f4
Alexis Saint-Jean <asaintjean@gmail.com>
2015-08-01 21:49:22 -0400

    FlashQuizFR: created a new VC, DictListVC, to hold the table with the available categories.

---
+++ FlashQuizFR/FlashQuizFR/DictListVC.swift
@@ -0,0 +1,116 @@
+//
+//  DictListVC.swift
+//  FlashQuizFR
+//
+//  Created by Alexis Saint-Jean on 8/1/15.
+//  Copyright (c) 2015 Alexis Saint-Jean. All rights reserved.
+//
+//CoreData code based on tutorial from http://www.raywenderlich.com/85578/first-core-data-app-using-swift
+//Group by function in CoreData based on http://stackoverflow.com/questions/29562104/ios-swift-core-data-counting-child-entities
+
+import UIKit
+import CoreData
+
+class DictListVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    var words = [AnyObject]()
+    var stringResultsArray = [AnyObject]()
+    var categoryFromList = ["category": String(), "wordCount": String()]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //        title = "\"The List\""
+        
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.stringResultsArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("wordCell") as! UITableViewCell
+        
+        var categoryName = self.stringResultsArray[indexPath.row]["category"] as! String
+        
+        cell.textLabel!.text = categoryName
+        cell.detailTextLabel?.text = self.stringResultsArray[indexPath.row]["wordCount"] as! String + " words"
+        
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 45
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //1
+        let appDelegate =
+        UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext!
+        
+        //2
+        
+        let fetchRequest = NSFetchRequest(entityName:"WordEntry")
+        
+        //sortDescriptor is to enable us to sort the list of category alphabetically, to display it in the tableView
+        let sortDescriptor = NSSortDescriptor(key: "category", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        let countExpression = NSExpression(format: "count:(wordFirst)")
+        let countED = NSExpressionDescription()
+        countED.expression = countExpression
+        countED.name = "wordCount"
+        countED.expressionResultType = .Integer32AttributeType
+        
+        fetchRequest.propertiesToFetch = ["category", countED]
+        //Here we are indicating that the fetchRequest will be an array of dictionaries
+        fetchRequest.resultType = .DictionaryResultType
+        fetchRequest.propertiesToGroupBy = ["category"]
+        
+        //3
+        var error: NSError?
+        
+        if let results = managedContext.executeFetchRequest(fetchRequest,
+            error: &error) {
+                words = results
+                println("TestVC: count is \(words.count)")
+                
+                for i in 0...results.count-1 {
+                    categoryFromList["category"] = words[i]["category"] as? String
+                    
+                    
+                    if let intCount = words[i]["wordCount"]! as? Int {
+                        categoryFromList["wordCount"] = String(intCount)
+                    }
+                    
+                    println("TestVC: \(categoryFromList)")
+                    self.stringResultsArray.append(categoryFromList)
+                }
+                println("TestVC: final array is \(self.stringResultsArray)")
+                
+                
+                
+                
+        } else {
+            println("Could not fetch \(error), \(error!.userInfo)")
+            
+        }
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
