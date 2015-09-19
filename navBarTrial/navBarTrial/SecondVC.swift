8d436a8a526156e046431754eb52a112d32d388a
Alexis Saint-Jean <asaintjean@gmail.com>
2015-09-19 05:32:42 -0400

    BarButtonItem: Adding a working arrow

--- navBarTrial/navBarTrial/SecondVC.swift
+++ navBarTrial/navBarTrial/SecondVC.swift
@@ -12,8 +12,51 @@ class SecondVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.hidesBackButton = true
+        
+        //1
+//        var goBackMenuButton = UIBarButtonItem(title: "Main Menu", style: .Plain , target: self, action: "goBackToMainMenu")
+//        goBackMenuButton.enabled = true
+//        self.navigationItem.setLeftBarButtonItem(goBackMenuButton, animated: true)
+        
+        
+        //2
+        var custombutton = UIButton.buttonWithType(.Custom)  as! UIButton
+        custombutton.setTitle("Menu", forState: .Normal)
+        var image = UIImage(named: "back_arrow.png")?.imageWithRenderingMode(.AlwaysTemplate)
+        custombutton.setImage(image, forState: .Normal)
+//        custombutton.sizeToFit()
+        custombutton.frame.size = CGSizeMake(10, 20)
+        custombutton.addTarget(self, action: "goBackToMainMenu", forControlEvents:.TouchUpInside)
+        var customBarButtonItem = UIBarButtonItem(customView: custombutton)
 
-        // Do any additional setup after loading the view.
+//        var customBarButtonItem = UIBarButtonItem(image: "back_arrow.png", style: .Plain, target: self, action: "goBackToMainMenu")
+//        self.navigationItem.leftBarButtonItem = customBarButtonItem
+
+//        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[[UIImage imageNamed:@"info.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]
+//            style:UIBarButtonItemStylePlain
+//            target:self
+//            action:@selector(info:)]
+        
+        
+
+        //3
+        // 1
+        var backArrowButtonItem:UIBarButtonItem = UIBarButtonItem(title: "Add", style: .Plain, target: self, action: "goBackToMainMenu")
+//        // 2
+        var leftTextButtonItem:UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Search, target: self, action: "goBackToMainMenu")
+//        // 3
+        self.navigationItem.setLeftBarButtonItems([customBarButtonItem,backArrowButtonItem], animated: true)
+//
+//        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+    }
+    
+    func goBackToMainMenu() {
+        self.navigationController?.popToRootViewControllerAnimated(true)
     }
 
     override func didReceiveMemoryWarning() {
